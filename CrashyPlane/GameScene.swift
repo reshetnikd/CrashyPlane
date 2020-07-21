29f5ed9b6cc8f89781241aab6a2626411bee9065
reshetnikd <dmitry.reshetnik@icloud.com>
2020-07-21 19:23:51 +0300

    Add sky, background and ground with parallax scrolling

--- CrashyPlane/GameScene.swift
+++ CrashyPlane/GameScene.swift
@@ -14,6 +14,9 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         createPlayer()
+        createSky()
+        createBackground()
+        createGround()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -32,5 +35,56 @@ class GameScene: SKScene {
         let animation = SKAction.animate(with: [playerTexture, nextPlayerTexture, anotherPlayerTexture, nextPlayerTexture], timePerFrame: 0.01)
         let endlessAnimation = SKAction.repeatForever(animation)
         player.run(endlessAnimation)
+    }
+    
+    func createSky() {
+        let topSky = SKSpriteNode(color: UIColor(hue: 0.55, saturation: 0.14, brightness: 0.97, alpha: 1), size: CGSize(width: frame.width, height: frame.height * 0.67))
+        let bottomSky = SKSpriteNode(color: UIColor(hue: 0.55, saturation: 0.16, brightness: 0.96, alpha: 1), size: CGSize(width: frame.width, height: frame.height * 0.33))
+        topSky.anchorPoint = CGPoint(x: 0.5, y: 1)
+        topSky.position = CGPoint(x: frame.midX, y: frame.height)
+        bottomSky.anchorPoint = CGPoint(x: 0.5, y: 1)
+        bottomSky.position = CGPoint(x: frame.midX, y: bottomSky.frame.height)
+        addChild(topSky)
+        addChild(bottomSky)
+        
+        topSky.zPosition = -40
+        bottomSky.zPosition = -40
+    }
+    
+    func createBackground() {
+        let backgroundTexture = SKTexture(imageNamed: "background")
+        
+        for i in 0 ... 1 {
+            let background = SKSpriteNode(texture: backgroundTexture)
+            background.zPosition = -30
+            background.anchorPoint = CGPoint.zero
+            background.position = CGPoint(x: (backgroundTexture.size().width * CGFloat(i)) - CGFloat(1 * i), y: 100)
+            addChild(background)
+            
+            let moveLeft = SKAction.moveBy(x: -backgroundTexture.size().width, y: 0, duration: 20)
+            let moveReset = SKAction.moveBy(x: backgroundTexture.size().width, y: 0, duration: 0)
+            let sequence = SKAction.sequence([moveLeft, moveReset])
+            let moveLoop = SKAction.repeatForever(sequence)
+            
+            background.run(moveLoop)
+        }
+    }
+    
+    func createGround() {
+        let groundTexture = SKTexture(imageNamed: "ground")
+        
+        for i in 0 ... 1 {
+            let ground = SKSpriteNode(texture: groundTexture)
+            ground.zPosition = -10
+            ground.position = CGPoint(x: (groundTexture.size().width / 2.0 + (groundTexture.size().width * CGFloat(i))), y: groundTexture.size().height / 2)
+            addChild(ground)
+            
+            let moveLeft = SKAction.moveBy(x: -groundTexture.size().width, y: 0, duration: 5)
+            let moveReset = SKAction.moveBy(x: groundTexture.size().width, y: 0, duration: 0)
+            let sequence = SKAction.sequence([moveLeft, moveReset])
+            let moveLoop = SKAction.repeatForever(sequence)
+            
+            ground.run(moveLoop)
+        }
     }
 }
